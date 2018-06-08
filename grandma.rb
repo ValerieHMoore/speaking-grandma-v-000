483ec300bd327cdd8c0102eda4a5ffee80c46090
Valerie Moore <periwinkle17@outlook.com>
2018-06-08 16:43:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
-  "HUH?! SPEAK UP, SONNY!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.downcase
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-  "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
 end
